c5db4000bbc2638c591d1b052227b400a3d41991
Arya-veer <f20200094@pilani.bits-pilani.ac.in>
2021-05-12 15:25:43 +0530

    req

--- Bits_queries/settings.py
+++ Bits_queries/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -148,4 +149,3 @@ LOGOUT_REDIRECT_URL='/home/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root', 'static')
